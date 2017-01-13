e89957654c64149a94fe6f7ba70a051d2df7756a
poppinyunhai <yiranyunhaisrh@gmail.com>
2017-01-13 11:05:09 +0800

    first commit

---
+++ lib/qcloud/sms.rb
@@ -0,0 +1,7 @@
+require "qcloud/sms/version"
+
+module Qcloud
+  module Sms
+    # Your code goes here...
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'qcloud/sms'
+
+require 'minitest/autorun'
